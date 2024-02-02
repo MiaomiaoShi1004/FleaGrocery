96e1e8b5c133ad03e8cce71e78ebbbecf7a22b05
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-02-02 19:01:51 +0000

    Initial Commit

---
+++ FleaGrocery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FleaGrocery
+//
+//  Created by Miaomiao Shi on 02/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worldddd!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
